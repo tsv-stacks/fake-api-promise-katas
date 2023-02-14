eb67172a0cca43e9a534441e04adcfc54f4450e1
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-02-14 19:28:30 +0000

    first kata complete

--- src/promise-katas-1.js
+++ src/promise-katas-1.js
@@ -1,20 +1,20 @@
-/* 
+/*
 
-DO NOT change the names of the functions in this file. 
+DO NOT change the names of the functions in this file.
 
-However, you will have to amend the body of the functions themselves in order to make the tests pass. 
+However, you will have to amend the body of the functions themselves in order to make the tests pass.
 
 Look at the Promise documentation on MDN
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
 
 You can create a new promise by using the Promise constructor. We use the 'new' keyword,
-followed by an uppercase 'Promise'. Invoke the Promise constructor like this 'Promise()' and pass in a callback function 
-'Promise(() => {}). 
+followed by an uppercase 'Promise'. Invoke the Promise constructor like this 'Promise()' and pass in a callback function
+'Promise(() => {}).
 The callback function in the Promise constructor always has a resolve function as the first
 parameter and a reject function as the second parameter, 'Promise((resolve, reject) => {}).
 Sometimes we shorten this to 'res' for 'resolve' and 'rej' for 'reject'. The name
-of these parameters can be whatever you want to call them, however they will always be a 
-resolve and reject function as that is how Promises have been built for us. 
+of these parameters can be whatever you want to call them, however they will always be a
+resolve and reject function as that is how Promises have been built for us.
 
 Example use:
 
@@ -30,74 +30,128 @@ Remember, a Promise can only resolve successfully, or reject when it fails to re
 
 // 1 Create a function that returns a resolving promise
 // Example:
-// when we call returnPromise like 'returnPromise()' we expect our function to use the 
+// when we call returnPromise like 'returnPromise()' we expect our function to use the
 // 'resolve' Promise callback function in order to resolve successfully
 
-const returnPromise = () => { };
+const returnPromise = () => {
+    return new Promise((resolve, _) => {
+        return resolve()
+    })
+};
 
 // 2 Create a function that returns a promise which returns 10 if it resolves succesfully
 // Example:
 // returnTen() resolves to the value 10
 
-const returnTen = () => { };
+const returnTen = () => {
+    return new Promise((resolve, _) => {
+        return resolve(10)
+    })
+
+};
 
 // 3 Create a function that returns a promise which returns 'string' if it resolves succesfully
 // Example:
 // returnString() resolves to the value 'string'
 
-const returnString = () => { };
+const returnString = () => {
+    return new Promise((resolve, _) => {
+        return resolve("string")
+    })
+};
 
 // 4 Create a function that returns a promise which returns an object with a key of name and value of 'Bob' if it resolves succesfully
 // Example:
 // returnBob() resolves to the value { name: 'Bob' }
 
-const returnBob = () => { };
+const returnBob = () => {
+    return new Promise((resolve, _) => {
+        return resolve({ name: 'Bob' })
+    })
+};
 
 // 5 Create a function that returns a promise which returns an array with 4 strings of 'eggs', 'apples', 'milk' and 'bread' if it resolves succesfully
 // Example:
 // returnList() resolves to the value ['eggs', 'apples', 'milk', 'bread']
 
-const returnList = () => { };
+const returnList = () => {
+    return new Promise((resolve, _) => {
+        return resolve(['eggs', 'apples', 'milk', 'bread'])
+    })
+};
 
 // 6 Create a function that returns a promise which returns "An error occurred" if it rejects
 // Example:
 // anError() rejects to "An error occurred"
 
-const anError = () => { };
+const anError = () => {
+    return new Promise((_, reject) => {
+        return reject("An error occurred")
+    })
+};
 
 // 7 Create a function that returns a promise which returns 666 if it rejects
 // Example:
 // theNumberOfTheBeast() rejects to the value 666
 
-const theNumberOfTheBeast = () => { };
+const theNumberOfTheBeast = () => {
+    return new Promise((_, reject) => {
+        return reject(666)
+    })
+};
 
 // 8 Create a function that returns a promise which returns an object with a key of error and value of 500 if it rejects
 // Example:
 // internalServerError() rejects to { error: 500 }
 
-const internalServerError = () => { };
+const internalServerError = () => {
+    return new Promise((_, reject) => {
+        return reject({ error: 500 })
+    })
+};
 
-// 9 Create a function that returns a promise which resolves to "happy" if the parameter is greater than 
+// 9 Create a function that returns a promise which resolves to "happy" if the parameter is greater than
 // or equal to 1 and rejects to "sad" if the value is less than 1
 // Example:
 // happySad(1) resolves to "happy"
 // happySad(0) rejects to "sad"
 
-const happySad = () => { };
+const happySad = (x) => {
+    return new Promise((resolve, reject) => {
+        if (x >= 1) {
+            return resolve('happy')
+        } else {
+            return reject('sad')
+        }
+    })
+};
 
-// 10 Create a function that returns a promise which resolves to "Yes. Luke, I am your father." if the 
+// 10 Create a function that returns a promise which resolves to "Yes. Luke, I am your father." if the
 // parameter is "Luke" and rejects to "Not your dad." if the value is anything else
 // Example:
 // amIYourFather() resolves to "Yes. Luke, I am your father."
 // amIYourFather() rejects to "Not your dad."
 
-const amIYourFather = () => { };
+const amIYourFather = (names) => {
+    return new Promise((resolve, reject) => {
+        if (names === "Luke") {
+            return resolve('Yes. Luke, I am your father.')
+        } else {
+            return reject("Not your dad.")
+        }
+    })
+};
 
-// 11 Create a function that returns a promise. The promise should resolve to a new function that returns "My name is <input name>" 
+// 11 Create a function that returns a promise. The promise should resolve to a new function that returns "My name is <input name>"
 // Example:
 // A call to myNameIs() resolves to a new function that takes one argument, name, and returns "My name is name" Eg (name) => `My name is ${name}`
 
-const myNameIs = () => { };
+const myNameIs = () => {
+    return new Promise((resolve, _) => {
+
+        return resolve((name) => `My name is ${name}`)
+    })
+};
 
 
 module.exports = {
--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -1,13 +1,13 @@
 const { fetch } = require("./fakeApi");
-/* 
+/*
 
-DO NOT change the import or names of the functions in this file. 
+DO NOT change the import or names of the functions in this file.
 
-However, you will have to amend the body of the functions themselves in order to make the tests pass. 
+However, you will have to amend the body of the functions themselves in order to make the tests pass.
 
 You can look at the fakeApi.js file to see the shape of the data that is returned from our fakeApi
 
-We have imitated the fetch module for these katas. The fetch function takes two parameters, url and requestBody. 
+We have imitated the fetch module for these katas. The fetch function takes two parameters, url and requestBody.
 
 For the first 3 exercises you will only need to provide the first parameter, url. For the last exercise you will
 need to use the second parameter, requestBody.
@@ -25,29 +25,32 @@ fetch("joke", "question").then(<insert your callback function>)
 // 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+const food = () => { };
 
 // 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = () => { };
 
 // 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = () => { };
 
 // 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
-// a joke object with the key of question and answer - expected return { 
+// a joke object with the key of question and answer - expected return {
 //     question: "Why did the scarecrow win the Nobel Prize?",
 //     answer: "Because he was out-standing in his field."
 // } of type Object
-// You will have to make more than one request to our fakeApi to get all the data you need. 
+// You will have to make more than one request to our fakeApi to get all the data you need.
 // Be aware of nesting your calls (try to avoid callback hell). Take a look at Promise.all on MDN
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 //
 
-const joke = () => {};
+const joke = () => {
+
+
+};
 
 module.exports = {
     food,
